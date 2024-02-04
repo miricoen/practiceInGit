569232c709462e5a77bb748cb3e4a8b1d2cc7fb1
hitmachut <hitmachut@students.local>
2024-02-04 13:57:47 +0200

    first commit by miri

---
+++ src/app/try/try.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-try',
+  standalone: true,
+  imports: [],
+  templateUrl: './try.component.html',
+  styleUrl: './try.component.css'
+})
+export class TryComponent {
+
+}
